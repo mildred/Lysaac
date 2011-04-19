2344fbb0c32e79a153f4fd921dd723ed6fc9a4f2
Mildred Ki'Lya <silkensedai@online.fr>
2011-04-19 19:54:04 +0200

    use our copy of stdlib

---
+++ stdlib/unstable/sdl-binding/gen_doc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+shorter -d -f html sdl/ -o docs/html
+#shorter -d -f latex sdl/ -o docs/latex
+shorter -d -f html_raw sdl/ -o docs/html_raw
+shorter -d -f belinda sdl/ -o docs/belinda
+
