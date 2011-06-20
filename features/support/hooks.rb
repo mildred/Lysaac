86df3f23370cb4a49c10de38dcb48219d0aed4c3
Mildred Ki'Lya <silkensedai@online.fr>
2011-06-20 17:46:23 +0200

    Fix légume vert

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,20 +6,22 @@ Before do
   dir = File.join($homedir, "tmp", "null")
   FileUtils.rm_rf dir
   FileUtils.mkdir_p dir
-  FileUtils.cd(dir)
+  FileUtils.cd dir
 end
 
 Around do |scenario, block|
   dir = File.join($homedir, "tmp", scenario.file_colon_line.gsub(/[\/\\:]/, "."))
   FileUtils.rm_rf dir
   FileUtils.mkdir_p dir
-  FileUtils.cd(dir) do
-    block.call
-  end
-  FileUtils.cd $oldcurrentdir
+  FileUtils.cd(dir)
+  block.call
   if scenario.failed?
     puts "Leftover files in #{dir}"
   else
     #FileUtils.rm_rf dir
   end
+end
+
+After do
+  FileUtils.cd $oldcurrentdir
 end
