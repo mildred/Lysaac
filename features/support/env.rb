7c271d20c4c74e752bd204262ddf043ca90c2374
Mildred Ki'Lya <silkensedai@online.fr>
2011-06-22 11:15:58 +0200

    Minor fixes, starting refactoring REG

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,16 @@
 require 'spec/expectations'
+require 'fileutils'
 
+$initcurrentdir = FileUtils.pwd
 $homedir = File.dirname(File.dirname(File.dirname(__FILE__)))
 $resourcedir = File.dirname(File.dirname(__FILE__)) + "/resources"
+$tmpdir = File.join($homedir, "tmp", "null")
 
+def cd_in_unique_dir
+  dir = if $scenariodir.nil? then $tmpdir else $scenariodir end
+  if FileUtils.pwd != dir then
+    FileUtils.rm_rf dir
+    FileUtils.mkdir_p dir
+    FileUtils.cd dir
+  end
+end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,26 +2,21 @@ require 'fileutils'
 
 Before do
   ENV['LYSAAC_PATH'] = nil
-  $oldcurrentdir = FileUtils.pwd
-  dir = File.join($homedir, "tmp", "null")
-  FileUtils.rm_rf dir
-  FileUtils.mkdir_p dir
-  FileUtils.cd dir
+  cd_in_unique_dir
 end
 
 Around do |scenario, block|
-  dir = File.join($homedir, "tmp", scenario.file_colon_line.gsub(/[\/\\:]/, "."))
-  FileUtils.rm_rf dir
-  FileUtils.mkdir_p dir
-  FileUtils.cd(dir)
+  $scenariodir = File.join($homedir, "tmp", scenario.file_colon_line.gsub(/[\/\\:]/, "."))
+  cd_in_unique_dir
   block.call
   if scenario.failed?
-    puts "Leftover files in #{dir}"
+    puts "Leftover files in #{$scenariodir}"
   else
-    #FileUtils.rm_rf dir
+    #FileUtils.rm_rf $scenariodir
   end
+  $scenariodir = nil
 end
 
 After do
-  FileUtils.cd $oldcurrentdir
+  FileUtils.cd $initcurrentdir
 end
